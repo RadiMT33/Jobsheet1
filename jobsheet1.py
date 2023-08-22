820253265110243b1f61d27c2fb47c8f74926fd5
RadiMT33 <113565405+RadiMT33@users.noreply.github.com>
2023-08-22 09:32:49 +0700

    Game Tebak Angka

---
+++ jobsheet1.py
@@ -0,0 +1,12 @@
+import random
+from guess import cek
+
+jawaban = random.randint(1,10)
+tebakan = int(input("Masukkan angka dari 1 s.d 10 : ")) 
+
+if cek(tebakan, jawaban):
+	print("jawaban kamu benar !!")
+	
+else:
+	print("jawaban kamu salah")
+	
